946142656454a90f2c5f8efc848f8fef98d73f56
LLOYD-QNX <john.lloyd_butay@questronix.com.ph>
2026-02-20 16:31:32 +0800

    feature/footer-buttons-hover

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -4,8 +4,8 @@ import { GoArrowUpRight } from "react-icons/go";
 
 export default function Footer() {
   return (
-    <div className="sticky h-[40rem] z-0 bg-black">
-      <div className="flex lg:absolute w-full flex-col bottom-0">
+    <div className="sticky flex flex-col bottom-0 h-[40rem] bg-black">
+      <div className="absolute bottom-0 w-full">
         <div className="flex flex-col mb-[4em] md:mb-[70px] lg:mb-[140px] px-[14px] md:px-[42px]">
           <div className="flex flex-col lg:flex-row lg:w-[70vw] lg:justify-between gap-[3em] md:gap-[2em]">
             <div className="flex flex-col mt-[84px] md:mt-0 gap-[.75em]">
@@ -15,7 +15,7 @@ export default function Footer() {
               <div className="flex flex-col gap-[.75em] lg:gap-[0.5em]">
                 <Link
                   href="mailto:johnlloydbutay123@gmail.com"
-                  className="text-white text-[18.2px] md:text-[21px] font-medium lg:leading-[25px] lg:tracking-[-0.2px]"
+                  className="text-white text-[18.2px] md:text-[21px] hover:text-[#ad7b00] font-extrabold transition-colors duration-300 delay-75 lg:leading-[25px] lg:tracking-[-0.2px]"
                   target="_blank"
                 >
                   johnlloydbutay123@gmail.com
@@ -29,29 +29,25 @@ export default function Footer() {
               <div className="flex flex-col gap-[.75em] lg:gap-[0.5em]">
                 <Link
                   href="/"
-                  className="text-white text-[18.2px] md:text-[21px] font-medium lg:leading-[25px] lg:tracking-[-0.2px]"
-                  target="_blank"
+                  className="text-white text-[18.2px] md:text-[21px] hover:text-[#ad7b00] font-extrabold transition-colors duration-300 delay-75 lg:leading-[25px] lg:tracking-[-0.2px]"
                 >
                   Home
                 </Link>
                 <Link
                   href="/projects"
-                  className="text-white text-[18.2px] md:text-[21px] font-medium lg:leading-[25px] lg:tracking-[-0.2px]"
-                  target="_blank"
+                  className="text-white text-[18.2px] md:text-[21px] hover:text-[#ad7b00] font-extrabold transition-colors duration-300 delay-75 lg:leading-[25px] lg:tracking-[-0.2px]"
                 >
                   Selected Works
                 </Link>
                 <Link
                   href="/about"
-                  className="text-white text-[18.2px] md:text-[21px] font-medium lg:leading-[25px] lg:tracking-[-0.2px]"
-                  target="_blank"
+                  className="text-white text-[18.2px] md:text-[21px] hover:text-[#ad7b00] font-extrabold transition-colors duration-300 delay-75 lg:leading-[25px] lg:tracking-[-0.2px]"
                 >
                   Info
                 </Link>
                 <Link
-                  href="/"
-                  className="text-white text-[18.2px] md:text-[21px] font-medium lg:leading-[25px] lg:tracking-[-0.2px]"
-                  target="_blank"
+                  href="/playground"
+                  className="text-white text-[18.2px] md:text-[21px] hover:text-[#ad7b00] font-extrabold transition-colors duration-300 delay-75 lg:leading-[25px] lg:tracking-[-0.2px]"
                 >
                   Playground
                 </Link>
@@ -64,14 +60,14 @@ export default function Footer() {
               <div className="flex flex-col gap-[.75em] lg:gap-[0.5em]">
                 <Link
                   href="https://www.linkedin.com/in/john-lloyd-butay-a7675917b/"
-                  className="text-white text-[18.2px] md:text-[21px] font-medium lg:leading-[25px] lg:tracking-[-0.2px]"
+                  className="text-white hover:text-[#ad7b00] font-extrabold transition-colors duration-300 delay-75 text-[18.2px] md:text-[21px] lg:leading-[25px] lg:tracking-[-0.2px]"
                   target="_blank"
                 >
                   Linkedin
                 </Link>
                 <Link
                   href="https://drive.google.com/drive/folders/16AhL2iwMCPaE-uFV_E3lstWNgAVkT6O5?usp=drive_link"
-                  className="text-white text-[18.2px] md:text-[21px] font-medium lg:leading-[25px] lg:tracking-[-0.2px]"
+                  className="text-white hover:text-[#ad7b00] font-extrabold transition-colors duration-300 delay-75 text-[18.2px] md:text-[21px] lg:leading-[25px] lg:tracking-[-0.2px]"
                   target="_blank"
                 >
                   My resume
@@ -94,52 +90,55 @@ export default function Footer() {
             <FaArrowRight size={30} color="black" />
           </a>
         </div>
-        <div className="hidden md:flex w-full items-center px-[42px] mb-[14px] lg:mb-[28px]">
-          <div className="lg:hidden mr-[32px] lg:mr-[48px] pb-[18px] lg:pb-[13.312px] text-[84px] lg:text-[6.6svw] text-white lg:text-[#888888] font-medium tracking-[-0.4vw] leading-[0.7em]">
+        <a
+          href="http://calendly.com/johnlloydbutay123/30min"
+          className="group hidden md:flex w-full items-center px-[42px] mb-[14px] lg:mb-[28px] hover:text-white"
+        >
+          <div className="lg:hidden mr-[32px] lg:mr-[48px] pb-[18px] lg:pb-[13.312px] text-[84px] lg:text-[6.6svw] text-[#888888] font-medium tracking-[-0.4vw] leading-[0.7em] group-hover:text-white">
             Let&apos;s create
           </div>
-          <div className="hidden lg:block mr-[32px] lg:mr-[48px] pb-[18px] lg:pb-[13.312px] text-[84px] lg:text-[6.6svw] text-white lg:text-[#888888] font-medium tracking-[-0.4vw] leading-[0.7em]">
+          <div className="hidden lg:block mr-[32px] lg:mr-[48px] pb-[18px] lg:pb-[13.312px] text-[84px] lg:text-[6.6svw] text-white lg:text-[#888888] font-medium tracking-[-0.4vw] leading-[0.7em] group-hover:text-white transition-colors duration-300 delay-75">
             Let&apos;s create together
           </div>
-          <div className="border-b-[1px] border-[#888888] flex-1" />
-          <div className="py-[14px] lg:py-[10.240px] pr-[14px] lg:pr-[10.240px] pl-[4em] lg:pl-[40.960px] ml-[32px] lg:ml-[42px] bg-white rounded-full">
+          <div className="border-b-[1px] border-[#888888] flex-1 group-hover:border-white transition-colors duration-300 delay-75" />
+          <div className="flex justify-end items-center h-[58.8px] py-[14px] lg:py-[10.240px] w-[103.99px] pr-[14px] lg:pr-[10.240px] pl-[4em] lg:pl-[40.960px] ml-[32px] lg:ml-[42px] bg-white group-hover:bg-[#dfb44b] rounded-full transition-colors duration-300 delay-75">
             <FaArrowRight size={32} color="black" />
           </div>
-        </div>
+        </a>
         <div className="flex flex-col md:h-[84px] md:flex-row md:justify-between md:items-center gap-[1em] py-[21px] pr-[14px] pl-[23.8px] md:px-[42px]">
           <div className="flex justify-center gap-[0.8em]">
             <a
               href=""
               target="_blank"
-              className="flex items-center gap-[0.5em] py-[11.2px] px-[7px] lg:px-[18.2px] lg:py-0 md:p-[11.2px]"
+              className="flex group items-center gap-[0.5em] py-[11.2px] px-[7px] lg:px-[18.2px] lg:py-0 md:p-[11.2px] rounded-full hover:bg-[#666666] transition-color duration-300 delay-75"
             >
-              <div className="text-[12px] md:text-[14px] text-[#888888] uppercase">
+              <div className="text-[12px] md:text-[14px] text-[#888888] group-hover:text-white uppercase transition-colors duration-300 delay-75">
                 <span className="lg:hidden">Tw</span>
                 <span className="hidden lg:block">Twitter</span>
               </div>
-              <GoArrowUpRight className="w-[14px] h-[14px] text-[#888888]" />
+              <GoArrowUpRight className="w-[14px] h-[14px] text-[#888888] group-hover:text-white transition-colors duration-300 delay-75" />
             </a>
             <a
               href=""
               target="_blank"
-              className="flex items-center gap-[0.5em] py-[11.2px] px-[7px] md:p-[11.2px]"
+              className="flex group items-center gap-[0.5em] py-[11.2px] px-[7px] md:p-[11.2px] rounded-full hover:bg-[#666666] transition-color duration-300 delay-75"
             >
-              <div className="text-[12px] md:text-[14px] text-[#888888] uppercase">
+              <div className="text-[12px] md:text-[14px] text-[#888888] group-hover:text-white uppercase transition-colors duration-300 delay-75">
                 <span className="lg:hidden">Be</span>
                 <span className="hidden lg:block">Behance</span>
               </div>
-              <GoArrowUpRight className="w-[14px] h-[14px] text-[#888888]" />
+              <GoArrowUpRight className="w-[14px] h-[14px] text-[#888888] group-hover:text-white transition-colors duration-300 delay-75" />
             </a>
             <a
               href=""
               target="_blank"
-              className="flex items-center gap-[0.5em] py-[11.2px] px-[7px] md:p-[11.2px]"
+              className="flex group items-center gap-[0.5em] py-[11.2px] px-[7px] md:p-[11.2px] rounded-full hover:bg-[#666666] transition-color duration-300 delay-75"
             >
-              <div className="text-[12px] md:text-[14px] text-[#888888] uppercase">
+              <div className="text-[12px] md:text-[14px] text-[#888888] group-hover:text-white uppercase transition-colors duration-300 delay-75">
                 <span className="lg:hidden">Li</span>
                 <span className="hidden lg:block">Linkedin</span>
               </div>
-              <GoArrowUpRight className="w-[14px] h-[14px] text-[#888888]" />
+              <GoArrowUpRight className="w-[14px] h-[14px] text-[#888888] group-hover:text-white transition-colors duration-300 delay-75" />
             </a>
           </div>
           <div className="flex justify-center items-center text-[14px] text-[#888888] uppercase">
