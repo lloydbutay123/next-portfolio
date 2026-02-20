0d220afa8b3b5749c9877a5952be1c2532f14330
LLOYD-QNX <john.lloyd_butay@questronix.com.ph>
2026-02-20 17:34:40 +0800

    Add smooth transition for floating modal button

--- src/components/common/ModalButton.js
+++ src/components/common/ModalButton.js
@@ -6,22 +6,27 @@ import { IoClose } from "react-icons/io5";
 import Modal from "./Modal";
 
 export default function ModalButton() {
-
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    return (
-        <>
-            <button className={`z-10 h-[72px] w-[72px] bg-[#888888] rounded-full ${ isModalOpen ? 'p-[12px] ' : 'p-[8px] ' }`} onClick={() => setIsModalOpen(prev => !prev) }>
-                <div className={`flex justify-center items-center rounded-full w-full h-full ${ isModalOpen ? 'bg-[#dfb44b]' : 'bg-white' } `}>
-                    {
-                        isModalOpen? (
-                            <IoClose className="w-[24px] h-[24px]" />
-                        ) : (
-                            <HiMenuAlt4 className="w-[24px] h-[24px]" color="#090909" />
-                        )
-                    }
-                </div>
-            </button>  
-            <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
-        </>
-    );
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  return (
+    <>
+      <button
+        className={`group z-10 h-[72px] w-[72px] bg-[#888888] rounded-full transition-all duration-300 ease-in-out ${isModalOpen ? "p-[12px] " : "p-[8px] "}`}
+        onClick={() => setIsModalOpen((prev) => !prev)}
+      >
+        <div
+          className={`flex justify-center items-center rounded-full w-full h-full transition-colors duration-300 ease-in-out ${isModalOpen ? "bg-[#dfb44b]" : "bg-white"} `}
+        >
+          {isModalOpen ? (
+            <IoClose className="w-[24px] h-[24px] transition-transform duration-300 ease-in-out transform rotate-180" />
+          ) : (
+            <HiMenuAlt4
+              className="w-[24px] h-[24px]"
+              color="#090909 transition-transform duration-300 ease-in-out transform rotate-0"
+            />
+          )}
+        </div>
+      </button>
+      <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
+    </>
+  );
 }
