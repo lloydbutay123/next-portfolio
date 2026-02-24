4fd0be6d921437d7f78bfbcbcf434cc9669531cb
John Lloyd Butay <122253549+lloydbutay123@users.noreply.github.com>
2026-02-25 00:32:46 +0800

    Update page.js

--- src/app/contact/success/page.js
+++ src/app/contact/success/page.js
@@ -3,10 +3,10 @@ import Link from "next/link";
 export default function ContactSuccessPage() {
   return (
     <section className="min-h-screen flex flex-col justify-center items-center bg-white">
-      <h1 className="text-4xl font-bold text-[#090909] mb-4">
+      <h1 className="text-4xl text-center font-bold text-[#090909] mb-4">
         Message Sent Successfully!
       </h1>
-      <p className="text-[#888888] mb-8">
+      <p className="text-[#888888] text-center mb-8">
         Thank you for contacting me. I will get back to you soon.
       </p>
       <Link href="/" className="text-[#dfb44b] font-bold underline">
